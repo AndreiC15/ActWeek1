123a7693cfef78668bf686c4b3ddc8da728ea3da
MaxPayne15 <128194624+AndreiC15@users.noreply.github.com>
2024-03-14 08:37:19 +0800

    Database sql added

--- logintest.sql
+++ logintest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2024 at 01:13 AM
+-- Generation Time: Mar 14, 2024 at 01:36 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -44,15 +44,6 @@ CREATE TABLE `user_acct` (
   `ProfilePic` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `user_acct`
---
-
-INSERT INTO `user_acct` (`ID`, `FirstName`, `MiddleName`, `LastName`, `Email`, `Password`, `PhoneNumber`, `Country`, `Province`, `CityCity`, `District`, `HouseNoStreet`, `ZipCode`, `ProfilePic`) VALUES
-(89, 'Christian Javen', 'Yuquimpo', 'Samson', 'tkhized@gmail.com', 'tkhized123', '09223333331', 'Canada', 'Manitoba', 'Winnipeg', 'The Exchange District', '102 Good Street', 'R0E-1J3', 'profilePic/Headbang.gif'),
-(90, 'Matthew', 'Tiotangco', 'Caldeorn', 'matthew18@gmail.com', 'matthew18', '99999999999', 'A', 'A', 'A', 'A', '0701 Talong Purok', '0213', ''),
-(91, 'Calderon', 'Calderon', 'Calderon', 'andreicalderon15@gmail.com', 'andreic15151', '22222222222', 'Aaa', 'Aaa', 'Aaa', 'Aaa', 'Aaa', '123', 'profilePic/cropped-1920-1080-1297017.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -64,14 +55,6 @@ CREATE TABLE `wallpaper` (
   `Title` varchar(255) NOT NULL,
   `WallpaperLocation` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `wallpaper`
---
-
-INSERT INTO `wallpaper` (`WallpaperID`, `Title`, `WallpaperLocation`) VALUES
-(47, 'Spider-Man', 'upload/789178.jpg'),
-(48, 'Pacific Rim Uprising', 'upload/cropped-1360-768-910645.jpg');
 
 --
 -- Indexes for dumped tables
